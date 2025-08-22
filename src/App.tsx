53a3c750ea34fabfcac9e2b4ca07a04fd75bfd9e
Maksym Maiboroda <maksym.majboroda@gmail.com>
2025-08-22 16:16:25 +0300

    feature/add-material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import './App.css'
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
-    <div>
-      <h1>Today's Wikipedia</h1>
-    </div>
-  )
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="sm">
+        <Box sx={{ height: '100vh' }} >
+          <Button variant="outlined">Button</Button>
+          <h1>Today's Wikipedia</h1>
+        </Box>
+      </Container>
+    </React.Fragment>
+  );
 }
 
-export default App
+export default App;
