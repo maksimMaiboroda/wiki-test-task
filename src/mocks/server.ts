f079d923ce652d289243337bc5120c7fd2aec8f3
Maksym Maiboroda <maksym.majboroda@gmail.com>
2025-08-28 18:18:30 +0300

    feature/fix-mock-server

--- src/mocks/server.ts
+++ src/mocks/server.ts
@@ -1,16 +1,14 @@
 import { setupServer } from 'msw/node';
-import { rest } from 'msw';
+import { http, HttpResponse } from 'msw';
 
 export const server = setupServer(
-  rest.get('https://en.wikipedia.org/w/api.php', (req, res, ctx) => {
-    return res(
-      ctx.json({
-        query: {
-          pages: {
-            123: { pageid: 123, title: 'Test', extract: 'Mocked extract' },
-          },
+  http.get('https://en.wikipedia.org/w/api.php', () => {
+    return HttpResponse.json({
+      query: {
+        pages: {
+          123: { pageid: 123, title: 'Test', extract: 'Mocked extract' },
         },
-      })
-    );
+      },
+    });
   })
 );
