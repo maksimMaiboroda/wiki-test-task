6a636072ca6626f8e39ad865d1e0503fb740bdfc
Maksym Maiboroda <maksym.majboroda@gmail.com>
2025-08-28 17:12:22 +0300

    feature/add-aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@modals': path.resolve(__dirname, './src/components/modals'),
+      '@ui': path.resolve(__dirname, './src/components/ui'),
+      '@services': path.resolve(__dirname, './src/services'),
+      '@router': path.resolve(__dirname, './src/router'),
+      '@store': path.resolve(__dirname, './src/store'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@layouts': path.resolve(__dirname, './src/layouts'),
+      '@config': path.resolve(__dirname, './src/config'),
+      '@theme': path.resolve(__dirname, './src/theme'),
+    },
+  },
+});
