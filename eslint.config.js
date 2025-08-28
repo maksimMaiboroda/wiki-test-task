dc935cddce33cef3ed33d0f7ea3f3532c7a7ebdb
Maksym Maiboroda <maksym.majboroda@gmail.com>
2025-08-28 14:26:36 +0300

    feature/add-linters-and-setup-test-env

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,5 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import prettier from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
 
 export default tseslint.config([
   globalIgnores(['dist']),
@@ -14,7 +10,14 @@ export default tseslint.config([
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      prettierConfig,
     ],
+    plugins: {
+      prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
